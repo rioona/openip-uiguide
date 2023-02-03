04c81731f8a8fb07f0813efd99a9ede385825435
yuna <lyn6264@naver.com>
2023-02-03 11:32:06 +0900

    scss 작업중

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,16 +3,54 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ui-basic',
+    title: '프로젝트이름',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      //pc
+      // { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      //반응형
+      { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no",},
+      { name: "format-detection", content: "telephone=no" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "사이트 내용",
+      },
+      {
+        hid: "og:type",
+        property: "og:type",
+        content: "website",
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "사이트 이름",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:"사이트 내용",
+      },
+      // {
+      //   hid: "og:image",
+      //   property: "og:image",
+      //   content: "",//사이트 이미지 url
+      // },
+      // {
+      //   hid: "og:url",
+      //   property: "og:url",
+      //   content: "", //메인 url
+      // },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "stylesheet ", href: "https://s3.ap-northeast-2.amazonaws.com/static.unicornranch.co.kr/common/css/animate.min.css" },
+      { rel: "stylesheet ", href: "https://s3.ap-northeast-2.amazonaws.com/static.unicornranch.co.kr/common/css/swiper.min.css" },
+      // { rel: "stylesheet ", href: "https://s3.ap-northeast-2.amazonaws.com/static.unicornranch.co.kr/common/css/common.css" },
     ]
   },
+  css: ["~/static/scss/style.scss"],
   /*
   ** Customize the progress bar color
   */
@@ -33,7 +71,41 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
-    }
-  }
+      if (isClient) {
+        config.devtool = 'source-map'
+      }
+    },
+    filenames: {
+      css:  process.env.NODE_ENV === 'production' ? 'css/[name].css' : '[name].css',
+    },
+    // extractCSS: process.env.NODE_ENV === 'production' ? true : false,
+    extractCSS: true,
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.(css|vue)$/,
+            chunks: 'all',
+            enforce: true
+          }
+        }
+      }
+    },
+    // extractCSS: process.env.NODE_ENV === "production" ? true : false,
+    postcss: {
+      plugins: {
+        autoprefixer: {
+          grid: process.env.NODE_ENV === 'production' ? true : false,
+          flexbox: process.env.NODE_ENV === 'production' ? true : false,
+        },
+      },
+    },
+  },
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    // "@nuxtjs/eslint-module",
+    "@nuxt/postcss8",
+  ],
 }
 
